77f4ca467eb42eaa51277a95fd4ca6b79b34701c
MichaelBVaughn <mvaughn@wisc.edu>
2015-12-04 19:54:06 -0600

    added bash script

---
+++ unpack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+tar -xvf tcasPackage.tgz
+cd tcasPackage
+cd c-tools
+make build-all
+cd ..
+cd reversePatcher
+make
+cd ..
+cd time
+make build
+cd ..
+cd tsl
+make build
+cd ..
+cd tcas/scripts
+./tcasExec.py
+
+
