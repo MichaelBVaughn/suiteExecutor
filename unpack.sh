2b30cf3a9ae1e2709f7450c1703ab2c500c196ad
MichaelBVaughn <mvaughn@wisc.edu>
2015-12-17 04:21:37 -0600

    finalized unpack script

--- unpack.sh
+++ unpack.sh
@@ -4,18 +4,16 @@ wget http://proxy.chtc.wisc.edu/SQUID/mvaughn/x-tools.tar.gz
 tar -xvzf x-tools.tar.gz
 rm x-tools.tar.gz
 
-wget http://proxy.chtc.wisc.edu/SQUID/mvaughn/tcl.tar.gz
-tar -xvzf tcl.tar.gz
-rm tcl.tar.gz
+#set the permissions so I can install everything
+find x-tools -name "*" | xargs chmod 777
 
-
-wget http://proxy.chtc.wisc.edu/SQUID/mvaughn/vim_1.0.tar
-tar -xvf vim_1.0.tar
-rm vim_1.0.tar
+wget http://proxy.chtc.wisc.edu/SQUID/mvaughn/vimPackageTest.tar
+tar -xvf vimPackageTest.tar
+rm vimPackageTest.tar
 
 #TCL/expect installs aren't relocatable. Luckily they build in ~1 minute.
-export CC=$_CONDOR_JOB_IWD/x-tools/x86_64-unknown-linux-gnu/x86_64-unknown-linux-gnu-gcc
-
+export CC=$_CONDOR_JOB_IWD/x-tools/x86_64-unknown-linux-gnu/bin/x86_64-unknown-linux-gnu-gcc
+export _CROSS_COMPILER_SYSROOT=$_CONDOR_JOB_IWD/x-tools/x86_64-unknown-linux-gnu/x86_64-unknown-linux-gnu/sysroot
 wget http://proxy.chtc.wisc.edu/SQUID/mvaughn/tcl8.5.18-src.tar.gz
 tar -xvzf tcl8.5.18-src.tar.gz
 rm tcl8.5.18-src.tar.gz
@@ -24,11 +22,8 @@ wget http://proxy.chtc.wisc.edu/SQUID/mvaughn/expect5.45.tar.gz
 tar -xvzf expect5.45.tar.gz
 rm expect5.45.tar.gz
 
-mkdir tcl
-mkdir expect
-
 cd tcl8.5.18/unix/
-./configure --prefix=$_CONDOR_JOB_IWD/tcl
+./configure --prefix=$_CROSS_COMPILER_SYSROOT
 make
 make install
 cd ../..
@@ -39,15 +34,23 @@ make
 make install
 cd ..
 
-export PATH=$_CONDOR_JOB_IWD/tcl/bin
+export PATH=$_CROSS_COMPILER_SYSROOT/bin:$PATH
 #Finish installing tcl/expect
+
+#Build ncurses so that we can build vim
+wget http://proxy.chtc.wisc.edu/SQUID/mvaughn/ncurses-5.9.tar.gz
+tar -xvzf ncurses-5.9.tar.gz
+rm ncurses-5.9.tar.gz
+
+cd ncurses-5.9
+./configure --with-build-cc=$CC --prefix=$_CROSS_COMPILER_SYSROOT
+make
+make install
+cd ..
 
 export PATH=$_CONDOR_JOB_IWD/x-tools/x86_64-unknown-linux-gnu/bin:$PATH
 
-
-
-tar -xvf tcasPackage.tar
-cd tcasPackage
+cd vimPackage
 cd c-tools
 make build-all
 cd ..
@@ -60,7 +63,7 @@ cd ..
 cd tsl
 make build
 cd ..
-cd tcas/scripts
-./tcasExec.py $1
+cd vim/scripts
+./vimExec.py $1 $2
 cd ../..
-diff tcas/source/tcas.c tcas/versions.alt/versions.orig/v41/tcas.c  >> "job-$1-result.txt" 
+
